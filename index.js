8a86a1693a3a7f1ed84fd1410d0c7d1d2c55df21
Levi Miller <paullevimiller@gmail.com>
2019-09-10 18:34:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+
+function submitData(name, email) {
+
+    const formData = {
+        name,
+        email
+    };
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then((resp) => resp.json())
+        .then(json => {
+            document.body.innerHTML = json["id"];
+        })
+        .catch(error => {
+            document.body.innerHTML = error.message;
+        });
+};
+
+submitData('Steve', 'steve@steve.steve');
